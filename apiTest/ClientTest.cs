955bb432db1e8e9417307a2f428ebb4e4c76a545
Brian Tifa <brian.tifa08@gmail.com>
2019-08-08 22:22:20 -0400

    prueba y error de diferentes unit testing

---
+++ apiTest/ClientTest.cs
@@ -0,0 +1,97 @@
+﻿using Model;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace apiTest
+{
+   public  class ClientTest
+    {
+
+        public class ClientServiceFake : IClientService
+        {
+            private readonly List<Client> _Client;
+
+            public ClientServiceFake()
+            {
+                _Client = new List<Client>()
+            {
+                new Client() { ClientId = 1,
+                    Name = "Jean Carlos", LastName="Arnaud", Pedidos = "Madera, marmol, cemento" },
+               new Client() { ClientId = 2,
+                    Name = "Brian", LastName="Tifa", Pedidos = "Madera, cemento" },
+               new Client() { ClientId = 1,
+                    Name = "luis", LastName="Orange ", Pedidos = "Tornillos, martillo, llave" },
+            };
+            }
+
+            public IEnumerable<Client> GetAll()
+            {
+                return _Client;
+            }
+            public Client Get(int id)
+            {
+                 var result = new Client();
+
+                try
+                {
+                    result = _Client.Single(x => x.ClientId == id);
+                }
+                catch (System.Exception)
+                {
+
+                }
+
+                return result;
+            }
+            public bool Add(Client model)
+            {
+                try
+            {
+                    _Client.Add(model);
+                    _Client.SaveChanges();
+                }
+            catch (System.Exception)
+                {
+                    return false;
+                }
+                return true;
+            }
+
+            public bool Update(Client model)
+            {
+                try
+                {
+                    var originalModel = _Client.Single(x =>
+                        x.ClientId == model.ClientId
+                    );
+
+                    originalModel.Name = model.Name;
+                    originalModel.LastName = model.LastName;
+                    originalModel.Pedidos = model.Pedidos;
+                    _Client.Update(originalModel);
+                    _Client.SaveChanges();
+                }
+                catch (System.Exception)
+                {
+                    return false;
+                }
+                return true;
+
+                public Client GetById(int id)
+            {
+                return _Client.Where(a => a.ClientId == id)
+                    .FirstOrDefault();
+            }
+
+            public void Delete(int id)
+            {
+                var existing = _Client.First(a => a.ClientId == id);
+                _Client.Delete(existing);
+            }
+        }
+
+    }
+}
---
+++ apiTest/ServiceMethodTest.cs
@@ -0,0 +1,85 @@
+﻿using api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using static apiTest.ClientTest;
+
+namespace apiTest
+{
+     public class ServiceMethodTest
+    {
+        ClientController _controller;
+        IClientService _service;
+
+        public ServiceMethodTest()
+        {
+            _service = new ClientServiceFake();
+            _controller = new ClientController(_service);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsOkResult()
+        {
+            // Act
+            var okResult = _controller.Get();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult.Result);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsAllItems()
+        {
+            // Act
+            var okResult = _controller.Get().Result as OkObjectResult;
+
+            // Assert
+            var items = Assert.IsType<List<Client>>(okResult.Value);
+            Assert.Equal(3, items.Count);
+        }
+
+        //GetById 
+        [Fact]
+        public void GetById_UnknownGuidPassed_ReturnsNotFoundResult()
+        {
+            // Act
+            var notFoundResult = _controller.Get(Guid.NewGuid());
+
+            // Assert
+            Assert.IsType<NotFoundResult>(notFoundResult.Result);
+        }
+
+        [Fact]
+        public void GetById_ExistingGuidPassed_ReturnsOkResult()
+        {
+            // Arrange
+            var testGuid = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200");
+
+            // Act
+            var okResult = _controller.Get(testGuid);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult.Result);
+        }
+
+        [Fact]
+        public void GetById_ExistingGuidPassed_ReturnsRightItem()
+        {
+            // Arrange
+            var testGuid = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200");
+
+            // Act
+            var okResult = _controller.Get(testGuid).Result as OkObjectResult;
+
+            // Assert
+            Assert.IsType<ShoppingItem>(okResult.Value);
+            Assert.Equal(testGuid, (okResult.Value as ShoppingItem).Id);
+        }
+    }
+}
+
+   
